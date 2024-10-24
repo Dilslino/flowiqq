5492cfdc94a7d836343e4fa1c34e5b1da19b2d10
dilslino <86566123+Dilslino@users.noreply.github.com>
2024-10-24 11:06:02 +0700

    Add files via upload

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Upgrade pip
 python3 -m pip install --upgrade pip
+
+# Install dependencies
 python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+
+# Collect static files
+python3 manage.py collectstatic --noinput || true
