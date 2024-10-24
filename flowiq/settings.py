2d8b8d9cd895fae6799bf7f700d02ecae93a0c7a
dilslino <86566123+Dilslino@users.noreply.github.com>
2024-10-24 11:40:44 +0700

    Update settings.py

--- flowiq/settings.py
+++ flowiq/settings.py
@@ -79,15 +79,12 @@ WSGI_APPLICATION = 'flowiq.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
-# Tambahkan ini untuk menggunakan PostgreSQL di Vercel
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -124,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -228,3 +225,7 @@ LOGGING = {
         },
     },
 }
+
+if os.environ.get('VERCEL_ENV') == 'production':
+    ALLOWED_HOSTS.append('.vercel.app')
+    DEBUG = False
