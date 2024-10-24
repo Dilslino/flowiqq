901643f463d530928a28b9a57da91a9c0d712f72
dilslino <86566123+Dilslino@users.noreply.github.com>
2024-10-24 12:26:07 +0700

    Add files via upload

--- vercel_build.sh
+++ vercel_build.sh
@@ -3,6 +3,9 @@ echo "Building project packages..."
 echo "Installing project requirements..."
 python3 -m pip install -r requirements.txt
 
+echo "Installing psycopg2-binary..."
+python3 -m pip install psycopg2-binary==2.9.6
+
 echo "Migrating Database..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
